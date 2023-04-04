91d9dc369c57be947538ba5b3db5143b75cecea2
DiodeCN <1069523953@qq.com>
2023-04-05 05:24:19 +0800

    theme

---
+++ src/theme.jsx
@@ -0,0 +1,24 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
+
+export { theme };
